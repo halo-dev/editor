81b4bbe649f05a49ee5ba7c0b4b37fcf8eebf143
Ryan Wang <i@ryanc.cc>
2022-02-26 17:50:25 +0800

    chore: add vite inspect plugin
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import { createVuePlugin } from 'vite-plugin-vue2'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
   plugins: [
+    Inspect(),
     createVuePlugin(),
     Components({
       resolvers: [IconsResolver()]
